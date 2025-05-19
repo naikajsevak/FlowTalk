a8130aacf2d3c6a613fb0a10e6a925a6b78bc823
naikaj sevak <naikajsevak98@gmail.com>
2025-05-19 21:52:29 +0530

    Add GroupeMessagesModel.java

---
+++ app/src/main/java/com/example/flowtalk/models/GroupeMessagesModel.java
@@ -0,0 +1,86 @@
+package com.example.flowtalk.models;
+
+public class GroupeMessagesModel {
+    private String uid, message, messageId, msgUrl, messageType, title, thumbnail;
+    private long timestamp;
+    private int feeling;
+
+    public GroupeMessagesModel(String uid, String message, long timestamp,int feeling) {
+        this.uid = uid;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.feeling=feeling;
+    }
+    public GroupeMessagesModel(){}
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMsgUrl() {
+        return msgUrl;
+    }
+
+    public void setMsgUrl(String msgUrl) {
+        this.msgUrl = msgUrl;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getFeeling() {
+        return feeling;
+    }
+
+    public void setFeeling(int feeling) {
+        this.feeling = feeling;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+}
